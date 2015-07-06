60ce36044b6706ab49b8a8962f63552284647921
yevgenii.sh <yevgenii.sh@sollutium.com>
2015-07-06 19:08:06 +0300

    fixed -80px issue

--- Orchard-1.9/src/Orchard.Web/Themes/Teebay.Theme/Scripts/teebay.js
+++ Orchard-1.9/src/Orchard.Web/Themes/Teebay.Theme/Scripts/teebay.js
@@ -1,6 +1,6 @@
 ﻿function redirectToStartCampaign() {
     var prot = window.location.protocol;
     var host = window.location.host;
-    var des = prot + "//"+host + "/Design";
+    var des = prot + "//" + host + "/Design";
     window.location = des;
 }
