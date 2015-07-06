de2e28d393840c9e1ab8d8c2f847db3bfd63acb9
keinlekan <keinlekan@gmail.com>
2015-07-06 15:11:46 +0300

    issue refs TP-32: redirect to TeeWizard from HomePage

---
+++ Orchard-1.9/src/Orchard.Web/Themes/Teebay.Theme/Scripts/teebay.js
@@ -0,0 +1,6 @@
+﻿function redirectToStartCampaign() {
+    var prot = window.location.protocol;
+    var host = window.location.host;
+    var des = prot + "//"+host + "/Design";
+    window.location = des;
+}
