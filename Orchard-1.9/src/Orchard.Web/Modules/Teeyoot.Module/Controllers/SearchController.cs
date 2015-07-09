dc676dc1e73642daf8b3a791cdb905e553790005
keinlekan <keinlekan@gmail.com>
2015-07-10 02:06:13 +0300

    fix home page

---
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using Orchard.Themes;
+using Orchard.UI.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Teebay.Module.Controllers
+{
+    [Themed]
+    public class SearchController : Controller
+    {
+        public ActionResult Index(string filtersearch)
+        {
+            
+            return View(filtersearch);
+        }
+    }
+}
