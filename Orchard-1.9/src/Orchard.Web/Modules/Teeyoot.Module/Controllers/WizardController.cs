389bd3d4e824c43391c38a582bb0437db91471c5
dmliakhovets <dimagalaxy@gmail.com>
2015-07-10 11:26:03 +0300

    issue refs #TP-51: wizard pages

---
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Controllers/WizardController.cs
@@ -0,0 +1,20 @@
+﻿using Orchard.Themes;
+using Orchard.UI.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Teeyoot.Module.Controllers
+{
+    [Themed]
+    public class WizardController : Controller
+    {
+        // GET: Wizard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Scripts/wizard.js
@@ -0,0 +1,56 @@
+﻿window.onload = function initWizard() {
+    var w = $(window).width();
+    var h = $(window).height();
+    var slides = $('.Slides > div');
+    $('.SlideContainer').css({ height: (h - 60) + 'px' });
+    $('.Slides').css({ width: slides.length + '00%' });
+    slides.css({ width: w + 'px' });
+
+    var pos = 0;
+    slide();
+}
+function slide() {
+    var w = $(window).width();
+    var h = $(window).height();
+    var slides = $('.Slides > div');
+    $('.SlideContainer').css({ height: (h - 60) + 'px' });
+    $('.Slides').css({ width: slides.length + '00%' });
+    slides.css({ width: w + 'px' });
+    var pos = 0;
+
+    $('.Left').click(function () {
+        pos--;
+        $('.Slides').stop().animate({ left: (pos * w) + 'px' });
+    });
+    $('.Right').click(function () {
+        pos++;
+        $('.Slides').stop().animate({ left: (pos * w) + 'px' });
+    });
+    $('#design').click(function () {
+        document.getElementById('goal').className = "flow-step";
+        document.getElementById('design').className = "flow-step active";
+        document.getElementById('description').className = "flow-step";
+        pos = 0;
+        $('.Slides').stop().animate({ left: (pos * w) + 'px' });
+    });
+    $('#goal').click(function () {
+
+        document.getElementById('goal').className = "flow-step active";
+        document.getElementById('design').className = "flow-step";
+        document.getElementById('description').className = "flow-step";
+
+        pos = -1;
+        $('.Slides').stop().animate({ left: (pos * w) + 'px' });
+    });
+    $('#description').click(function () {
+        document.getElementById('goal').className = "flow-step";
+        document.getElementById('design').className = "flow-step";
+        document.getElementById('description').className = "flow-step active";
+        pos = -2;
+        $('.Slides').stop().animate({ left: (pos * w) + 'px' });
+    });
+}
+function onChangeTrackBar() {
+    document.getElementById('trackBarValue').innerHTML = document.getElementById('trackbar').value;
+    document.getElementById('total_profit').innerHTML = (document.getElementById('trackbar').value) * 10 + "$";
+}
