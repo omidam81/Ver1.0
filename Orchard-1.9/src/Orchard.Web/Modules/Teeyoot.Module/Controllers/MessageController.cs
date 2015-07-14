814759bf9615d80c2100d5f59c93d8ccf672b43d
dmliakhovets <dimagalaxy@gmail.com>
2015-07-14 22:05:30 +0300

    issue refs TP-60: interface and controller for messaging

--- Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Controllers/MessageController.cs
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Controllers/MessageController.cs
@@ -8,25 +8,15 @@ using System.Web.Mvc;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web.Script.Serialization;
+using Teeyoot.Module.Models;
+using MailChimp;
+using MailChimp.Campaigns;
+using MailChimp.Helper;
+using MailChimp.Lists;
 
 
 namespace Teeyoot.Module.Controllers
 {
-    public class MergeFields
-    {
-        public string FNAME;
-		public string LNAME;
-        public string CAMPAIGNID;
-    };
-    public class Member
-    {
-        public string email_address;
-        public string email_type;
-	    public string status;
-	public MergeFields merge_fields;
-	public bool vip;
-    public string listid;
-    };
 
     [Themed]
     public class MessageController : Controller
@@ -36,50 +26,40 @@ namespace Teeyoot.Module.Controllers
         {
             return View();
         }
+
+        [HttpGet]
         public ActionResult CreateMessage()
-        {
+        {           
             return View();
         }
-        public ActionResult getList()
+        public ActionResult SendMessage(MessageContentRecord m)
         {
-            using (var http = new HttpClient())
+            if (TryUpdateModel(m))
             {
-                http.DefaultRequestHeaders.Authorization =
-                     new AuthenticationHeaderValue("Basic", "efaebe4525defd02eec94579dfc7f4ce-us11");
-                var member = new Member()
+
+                MailChimpManager mc = new MailChimpManager("efaebe4525defd02eec94579dfc7f4ce-us11");
+                CampaignCreateOptions options = new CampaignCreateOptions() { };
+                options.FromEmail = m.From;
+                options.FromName = "Teeyoot seller";
+                options.Subject = m.Subject;
+                options.ListId = "f90603a2b0";
+                CampaignCreateContent content = new CampaignCreateContent() { };
+                content.Text = "vcx";
+                Campaign myCampaign = mc.CreateCampaign("regular", options, content);
+                CampaignContent newCont = new CampaignContent() { };
+                newCont.Text = m.Content;
+                CampaignUpdateResult upd = mc.UpdateCampaign(myCampaign.Id, "content", newCont);
+                List<string> emails = new List<string>();
+                emails.Add(m.Email);
+                CampaignActionResult response = mc.SendCampaignTest(myCampaign.Id, emails, "Text");
+                if (response.Complete)
                 {
-                    email_address = "test200s@mail.ru",
-                    email_type = "html",
-                    status = "subscribed",
-                    merge_fields = new MergeFields()
-                    {
-                        FNAME = "Test",
-                        LNAME = "Tester",
-                        CAMPAIGNID = "#100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000##100000000#"
-                    },
-                    vip = false,
-                    listid = "f90603a2b0",
-                };
-                string json = new JavaScriptSerializer().Serialize(member);
-                var stringContent = new StringContent(json);
-              
-                
-                //StringContent cont = new StringContent(@"{
-                //                ""email_address"":""me""}");
-                //var request = new HttpRequestMessage(new HttpMethod("PATCH"), @"https://us11.api.mailchimp.com/3.0/lists/f90603a2b0/members/fe1f141fa32b6592b1fca4d8eca2a546")
-                //{
-                //    Content = stringContent,
-                //};
-                //System.Threading.Tasks.Task<HttpResponseMessage> response = http.SendAsync(request);
-                //System.Threading.Tasks.Task<HttpResponseMessage> response = http.DeleteAsync(@"https://us11.api.mailchimp.com/3.0/lists/f90603a2b0/members/fe1f141fa32b6592b1fca4d8eca2a546");
-                //System.Threading.Tasks.Task<HttpResponseMessage> response = http.PostAsync(@"https://us11.api.mailchimp.com/3.0/lists/f90603a2b0/members", stringContent);
-                System.Threading.Tasks.Task<HttpResponseMessage> response = http.PostAsync(@"https://us11.api.mailchimp.com/3.0/campaigns", stringContent);
-               // System.Threading.Tasks.Task<string> content = http.GetStringAsync(@"https://us11.api.mailchimp.com/3.0/lists/f90603a2b0/members");
-               // ViewBag.resp = content.Result;
-                //Console.WriteLine(content.Result);
-               ViewBag.resp = response.Result.StatusCode;
+                    ViewBag.status = "Your message has been sended!";
+
+                }
+                return View("Index", ViewBag);
             }
-            return View();
+            return View("CreateMessage");
         }
     }
 }
