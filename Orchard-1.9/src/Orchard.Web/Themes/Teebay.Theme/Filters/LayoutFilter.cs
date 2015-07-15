865086f5394e256bcc1e1b402efbc56b62ef6903
yevgenii.sh <yevgenii.sh@sollutium.com>
2015-07-15 14:32:38 +0300

    layout for seller dashboard

---
+++ Orchard-1.9/src/Orchard.Web/Themes/Teebay.Theme/Filters/LayoutFilter.cs
@@ -0,0 +1,47 @@
+﻿using Orchard;
+using Orchard.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Teebay.Theme.Filters
+{
+    public class LayoutFilter : FilterProvider, IResultFilter
+    {
+        private readonly IWorkContextAccessor _wca;
+
+        public LayoutFilter(IWorkContextAccessor wca)
+        {
+            _wca = wca;
+        }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            var workContext = _wca.GetContext();
+            var routeValues = filterContext.RouteData.Values;
+            workContext.Layout.Metadata.Alternates.Add(
+                BuildShapeName(routeValues, "area"));
+            workContext.Layout.Metadata.Alternates.Add(
+                BuildShapeName(routeValues, "area", "controller"));
+            workContext.Layout.Metadata.Alternates.Add(
+                BuildShapeName(routeValues, "area", "controller", "action"));
+        }
+
+        public void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+        }
+
+        private static string BuildShapeName(
+            RouteValueDictionary values, params string[] names)
+        {
+
+            return "Layout__" +
+                string.Join("__",
+                    names.Select(s =>
+                        ((string)values[s] ?? "").Replace(".", "_")));
+        }
+    }
+}
