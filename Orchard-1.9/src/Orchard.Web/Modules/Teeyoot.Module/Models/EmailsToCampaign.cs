59fed0442bee2c5f409e3a522bd823fdc24bc537
dmliakhovets <dimagalaxy@gmail.com>
2015-07-15 17:20:30 +0300

    issue refs TP-60: fix styles and links

---
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Models/EmailsToCampaign.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Teeyoot.Module.Models
+{
+    public class EmailsToCampaign
+    {
+        public virtual int Id { get; set; }
+
+        public virtual int CampaignRecord_Id { get; set; }
+
+        public virtual string Email { get; set; }
+    }
+}
