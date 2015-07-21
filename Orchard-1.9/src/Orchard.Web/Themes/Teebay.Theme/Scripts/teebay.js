b66d328215626f54f7edca442aa78438f0fd2d8b
yevgenii.sh <yevgenii.sh@sollutium.com>
2015-07-21 22:56:19 +0300

    TP-71 updates

--- Orchard-1.9/src/Orchard.Web/Themes/Teebay.Theme/Scripts/teebay.js
+++ Orchard-1.9/src/Orchard.Web/Themes/Teebay.Theme/Scripts/teebay.js
@@ -11,3 +11,16 @@ function redirectToSearchCampaign() {
     var des = prot + "//" + host + "/TSearch?filter=";
     window.location = des;
 }
+
+var toggleMenu = function () {
+    var el = $('.tb-aside-first');
+
+    if (el.hasClass('menu-closed')) {
+        el.removeClass('menu-closed');
+    } else {
+        el.addClass('menu-closed');
+    }
+
+    $('.fa-angle-left').toggleClass('hidden');
+    $('.fa-angle-right').toggleClass('hidden');
+}
