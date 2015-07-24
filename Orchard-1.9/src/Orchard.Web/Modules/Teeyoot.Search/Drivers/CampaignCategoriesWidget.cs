a6af435cb3692cb7a05c73fae19d5761cf7f9cf7
keinlekan <keinlekan@gmail.com>
2015-07-24 09:54:53 +0300

    issue refs TP-49: add categories to the homepage

---
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Search/Drivers/CampaignCategoriesWidget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orchard.ContentManagement.Drivers;
+using Teeyoot.Module.Models;
+using Teeyoot.Search.Services;
+
+namespace Teeyoot.Search.Drivers
+{
+    public class CampaignCategoriesWidget : ContentPartDriver<CampaignCategoriesWidgetPart>
+    {
+        private readonly ICampaignCategoriesService _campaignCategory;
+
+        public CampaignCategoriesWidget(ICampaignCategoriesService campaignCategory)
+        {
+            _campaignCategory = campaignCategory;
+        }
+
+        protected override DriverResult Display(CampaignCategoriesWidgetPart part, string displayType, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_CampaignCategoriesWidget", () => shapeHelper.Parts_CampaignCategoriesWidget(Providers: _campaignCategory.GetAllCategories().ToList()));
+        }
+    }
+}
