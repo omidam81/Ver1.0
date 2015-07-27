dc2aef9c5a92995244d20a1f73cd434fce7e7242
maryna.p <maryna.p@sollutium.com>
2015-07-27 19:01:04 +0300

    issue refs 88: Page for Storefront creation

--- Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Dashboard/Controllers/DashboardStorefrontsController.cs
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Dashboard/Controllers/DashboardStorefrontsController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Web.Mvc;
+using Teeyoot.Dashboard.ViewModels;
+using Teeyoot.Module.Models;
+using System.Linq;
 
 namespace Teeyoot.Dashboard.Controllers
 {
     public partial class DashboardController : Controller
     {
+
         public ActionResult Storefronts()
         {
             return View();
@@ -11,7 +15,19 @@ namespace Teeyoot.Dashboard.Controllers
 
         public ActionResult NewStorefront()
         {
-            return View();
+            var user = _wca.GetContext().CurrentUser;
+            var teeyootUser = user.ContentItem.Get(typeof(TeeyootUserPart));
+            var campaigns = _campaignService.GetCampaignsOfUser(teeyootUser.Id).ToList();
+            var model = new StoreViewModel();
+            model.Campaigns = campaigns;
+            return View(model);
+        }
+
+        public ActionResult CreateStorefront(StoreViewModel model)
+        {
+            //var newStore = _storeService.CreateStore(store);
+
+            return View("NewStorefront");
         }
        
 
