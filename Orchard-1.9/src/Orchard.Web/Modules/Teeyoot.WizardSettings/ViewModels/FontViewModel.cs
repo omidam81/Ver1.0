38535bc042856ad0aaf7b34f70adb84120c05306
dmliakhovets <dimagalaxy@gmail.com>
2015-07-27 21:59:40 +0300

    issue refs #77: fonts Admin page

---
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.WizardSettings/ViewModels/FontViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Teeyoot.WizardSettings.ViewModels
+{
+    public class FontViewModel
+    {
+        public int Id { get; set; }
+       
+        public string Family { get; set; }
+        
+        public string FileName { get; set; }
+
+        public string Tags { get; set; }
+
+        public int? Priority { get; set; }
+
+        public string TtfFile { get; set; }
+
+        public string WoffFile { get; set; }
+
+        public string Thumbnail { get; set; }
+
+
+
+    }
+}
