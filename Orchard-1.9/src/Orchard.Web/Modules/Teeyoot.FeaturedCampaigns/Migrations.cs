9d68a4c610c7930e5250b706c50609dc90d971ab
keinlekan <keinlekan@gmail.com>
2015-07-27 13:14:51 +0300

    issue refs TP-108: Creating module Featured Campaigns

---
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.FeaturedCampaigns/Migrations.cs
@@ -0,0 +1,27 @@
+﻿using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+
+namespace Teeyoot.FeaturedCampaigns
+{
+    public class Migrations : DataMigrationImpl
+    {
+        public int Create()
+        {
+            ContentDefinitionManager.AlterPartDefinition(
+                "CampaignCategoriesWidgetPart",
+                builder => builder.Attachable());
+
+            ContentDefinitionManager.AlterTypeDefinition(
+                "CampaignCategoriesWidget",
+                cfg => cfg
+                           .WithPart("CampaignCategoriesWidgetPart")
+                           .WithPart("CommonPart")
+                           .WithPart("WidgetPart")
+                           .WithSetting("Stereotype", "Widget")
+                );
+
+            return 1;
+        }
+    }
+}
