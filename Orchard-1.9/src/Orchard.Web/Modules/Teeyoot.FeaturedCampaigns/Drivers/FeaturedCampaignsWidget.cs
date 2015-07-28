3e68072b0f899ea74010010d26b443090a37bbbb
keinlekan <keinlekan@gmail.com>
2015-07-28 13:20:00 +0300

    issue refs TP-108: created widget and fixed bugs

--- Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.FeaturedCampaigns/Drivers/FeaturedCampaignsWidget.cs
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.FeaturedCampaigns/Drivers/FeaturedCampaignsWidget.cs
@@ -17,5 +17,59 @@ namespace Teeyoot.FeaturedCampaigns.Drivers
         {
             _featuredCampaignsService = featuredCampaignsService;
         }
+
+        protected override DriverResult Display(FeaturedCampaignsWidgetPart part, string displayType, dynamic shapeHelper)
+        {
+            var campaignsInFeatured = _featuredCampaignsService.GetCampaignsFromAdmin().OrderByDescending(c => c.ProductCountSold).ToList();
+            var featuredCampaigns = new List<CampaignRecord>();
+
+            if (campaignsInFeatured.Count >= 6)
+            {
+                featuredCampaigns = campaignsInFeatured;
+            }
+            else
+            {
+                featuredCampaigns = campaignsInFeatured;
+                int countTopCamp = 6 - campaignsInFeatured.Count;
+                var ordersFromOneDay = _featuredCampaignsService.GetOrderForOneDay();
+                if (ordersFromOneDay != null && ordersFromOneDay.Count > 0)
+                {
+                    int[] ordersIdFromOneDay = ordersFromOneDay.Select(c => c.Id).ToArray();
+                    Dictionary<CampaignRecord, int> campaignsFromOrderForDay = _featuredCampaignsService.GetCampaignsFromOrderForOneDay(ordersIdFromOneDay);
+
+                    int take = campaignsFromOrderForDay.Count > 12 ? 12 : campaignsFromOrderForDay.Count;
+                    campaignsFromOrderForDay = campaignsFromOrderForDay.OrderByDescending(c => c.Value).OrderBy(c => c.Key.Title).Skip(0).Take(take).ToDictionary(p => p.Key, p => p.Value);
+
+                    Random rand = new Random();
+                    int insertCamp = campaignsFromOrderForDay.Count() <= countTopCamp ? campaignsFromOrderForDay.Count() : countTopCamp;
+                    for (int i = 0; i < insertCamp; i++)
+                    {
+                        featuredCampaigns.Add(campaignsFromOrderForDay.ElementAt(rand.Next(take)).Key);
+                    }
+                }
+                
+                if (featuredCampaigns.Count() < 6)
+                {
+                    countTopCamp = 6 - featuredCampaigns.Count();
+                    var otherCampaigns = _featuredCampaignsService.GetAllCampaigns().ToList();
+                    foreach (var camp in campaignsInFeatured)
+                    {
+                        if (otherCampaigns.Exists(c => c.Id == camp.Id) != null)
+                        {
+                            otherCampaigns.Remove(camp);
+                        }
+                    }
+                    int max = otherCampaigns.Count();
+
+                    Random rand = new Random();
+                    for (int i = 0; i < countTopCamp; i++)
+                    {
+                        featuredCampaigns.Add(otherCampaigns.ElementAt(rand.Next(max)));
+                    }
+                }
+            }
+
+            return ContentShape("Parts_FeaturedCampaignsWidget", () => shapeHelper.Parts_FeaturedCampaignsWidget(Campaigns: featuredCampaigns));
+        }
     }
 }
