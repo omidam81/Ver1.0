541338cd761be30e2975e11791a9fc23bdf56162
keinlekan <keinlekan@gmail.com>
2015-07-29 11:38:57 +0300

    issue refs TP-81: new change

--- Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Controllers/AdminCostController.cs
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Controllers/AdminCostController.cs
@@ -4,22 +4,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Teeyoot.Module.Models;
 using Teeyoot.Module.ViewModels;
+using Teeyoot.Module.Services.Interfaces;
 
 namespace Teeyoot.Module.Controllers
 {
     [Admin]
     public class AdminCostController : Controller
     {
+        private readonly ITShirtCostService _costService;
+
+        public AdminCostController(ITShirtCostService costService)
+        {
+            _costService = costService;
+        }
+
         // GET: AdminCost
         public ActionResult Index()
         {
-            return View("Index", new AdminCostViewModel {  });
+            TShirtCostRecord cost = _costService.GetCost();
+
+            if (cost == null)
+            {
+                return View("Index", new AdminCostViewModel { });
+            }
+            else
+            {
+                AdminCostViewModel costViewModel = new AdminCostViewModel { AdditionalScreenCosts = cost.AdditionalScreenCosts, CostOfMaterial = cost.CostOfMaterial, DTGPrintPrice = cost.DTGPrintPrice, FirstScreenCost = cost.FirstScreenCost, InkCost = cost.InkCost, LabourCost = cost.LabourCost, LabourTimePerColourPerPrint = cost.LabourTimePerColourPerPrint, LabourTimePerSidePrintedPerPrint = cost.LabourTimePerSidePrintedPerPrint, PercentageMarkUpRequired = cost.PercentageMarkUpRequired, PrintsPerLitre = cost.PrintsPerLitre };
+
+                return View("Index", costViewModel);
+            }
         }
 
         public ActionResult Edit()
         {
-            return View("Edit");
+            TShirtCostRecord cost = _costService.GetCost();
+            if (cost == null)
+            {
+                return View("Edit");
+            }
+            else
+            {
+                AdminCostViewModel costViewModel = new AdminCostViewModel { AdditionalScreenCosts = cost.AdditionalScreenCosts, CostOfMaterial = cost.CostOfMaterial, DTGPrintPrice = cost.DTGPrintPrice, FirstScreenCost = cost.FirstScreenCost, InkCost = cost.InkCost, LabourCost = cost.LabourCost, LabourTimePerColourPerPrint = cost.LabourTimePerColourPerPrint, LabourTimePerSidePrintedPerPrint = cost.LabourTimePerSidePrintedPerPrint, PercentageMarkUpRequired = cost.PercentageMarkUpRequired, PrintsPerLitre = cost.PrintsPerLitre };
+
+                return View("Edit", costViewModel);
+            }
         }
 
         public ActionResult Save(AdminCostViewModel costViewModel)
---
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Services/Interfaces/ITShirtCostService.cs
@@ -0,0 +1,19 @@
+﻿using Orchard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teeyoot.Module.Models;
+
+namespace Teeyoot.Module.Services.Interfaces
+{
+    public interface ITShirtCostService : IDependency
+    {
+        TShirtCostRecord GetCost();
+
+        bool UpdateCost(TShirtCostRecord cost);
+
+        bool InsertCost(TShirtCostRecord cost);
+    }
+}
