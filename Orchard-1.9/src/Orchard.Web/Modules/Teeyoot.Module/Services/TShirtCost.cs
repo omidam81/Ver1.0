2b420a4a10326d2d3db543098da9ef1c727f4c86
keinlekan <keinlekan@gmail.com>
2015-07-29 09:45:05 +0300

    fix error validation in the login page

---
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Services/TShirtCost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Teeyoot.Module.Services.Interfaces;
+
+namespace Teeyoot.Module.Services
+{
+    public class TShirtCost : ITShirtCost
+    {
+
+    }
+}
