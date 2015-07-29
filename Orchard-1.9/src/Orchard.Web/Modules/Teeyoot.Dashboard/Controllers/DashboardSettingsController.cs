be61113ec7b77b72a873cc4b059d1795afbe8403
dmliakhovets <dimagalaxy@gmail.com>
2015-07-29 13:34:18 +0300

    issue refs #TP-90: Settings layout

--- Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Dashboard/Controllers/DashboardSettingsController.cs
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Dashboard/Controllers/DashboardSettingsController.cs
@@ -1,11 +1,26 @@
-﻿using System.Web.Mvc;
+﻿using Orchard;
+using Orchard.Security;
+using System.Web.Mvc;
 
 namespace Teeyoot.Dashboard.Controllers
 {
     public partial class DashboardController : Controller
     {
+
+        private readonly IMembershipService _membershipService;
+       // private readonly IWorkContextAccessor _wca;
+
+
+        public DashboardController(IMembershipService membershipService, IWorkContextAccessor wca)
+        {
+            _membershipService = membershipService;
+        }
+        
+        
         public ActionResult Settings()
         {
+            var user = _wca.GetContext().HttpContext.User; 
+            
             return View();
         }
     }
