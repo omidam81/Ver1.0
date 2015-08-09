6a2fbc3d53b9ae9ae7b6038797378b844324d2e9
dmliakhovets <dimagalaxy@gmail.com>
2015-08-09 16:08:12 +0300

    issue refs #TP-83: Messages

--- Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Messaging/ViewModels/MailChimpListViewModel.cs
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Messaging/ViewModels/MailChimpListViewModel.cs
@@ -18,17 +18,35 @@ namespace Teeyoot.Messaging.ViewModels
 
         public virtual string RelaunchTemplate { get; set; }
 
-        public virtual string ChangeOrderStatusTemplate { get; set; }
-
-        public virtual string AllBuyersDeadlineTemplate { get; set; }
-
-        public virtual string WithdrawTemplate { get; set; }
-
         public virtual string LaunchTemplate { get; set; }
 
+        public virtual string CampaignIsPrintingSellerTemplate { get; set; }
+
+        public virtual string OrderIsPrintingBuyerTemplate { get; set; }
+
+        public virtual string CancelledOrderTemplate { get; set; }
+
+        public virtual string DeliveredOrderTemplate { get; set; }
+
+        public virtual string ShippedOrderTemplate { get; set; }
+
+        public virtual string NewOrderTemplate { get; set; }
+    
         public virtual string PlaceOrderTemplate { get; set; }
 
+        public virtual string CampaignNotReachGoalSellerTemplate { get; set; }
+
+        public virtual string CampaignNotReachGoalBuyerTemplate { get; set; }
+
+        public virtual string PaidCampaignTemplate { get; set; }
+
+        public virtual string UnpaidCampaignTemplate { get; set; }
+
+        public virtual string PartiallyPaidCampaignTemplate { get; set; }
+
         public virtual string CampaignPromoTemplate { get; set; }
+
+        public virtual string WithdrawTemplate { get; set; }
 
         public virtual string Culture { get; set; }
 
