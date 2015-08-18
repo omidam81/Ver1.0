0fa72c052cf566f7636b3920d076e13a116b9da5
keinlekan <keinlekan@gmail.com>
2015-08-18 11:51:46 +0300

    fixed delete categories

--- Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Models/CampaignCategoriesRecord.cs
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Models/CampaignCategoriesRecord.cs
@@ -12,12 +12,5 @@ namespace Teeyoot.Module.Models
         public virtual string Name { get; set; }
 
         public virtual bool IsVisible { get; set; }
-
-        public virtual IList<LinkCampaignAndCategoriesRecord> Campaigns { get; set; }
-
-        public CampaignCategoriesRecord()
-        {
-            Campaigns = new List<LinkCampaignAndCategoriesRecord>();
-        }
     }
 }
