3fac5db0903f8196e0416fa34ff6dd6191032a11
Oleg Ivanov <oleg.ivanov.nabusteam@gmail.com>
2015-08-19 17:10:47 +0300

    Getting truly random arts every time wizard loads

---
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Models/ArtItemDTO.cs
@@ -0,0 +1,10 @@
+﻿// ReSharper disable InconsistentNaming
+namespace Teeyoot.Module.Models
+{
+    public class ArtItemDto
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string filename { get; set; }
+    }
+}
