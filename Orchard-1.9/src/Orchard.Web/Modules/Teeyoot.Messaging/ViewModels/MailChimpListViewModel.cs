22094615bbe0aac35cc8f0d8ea9bad61d29aa730
dmliakhovets <dimagalaxy@gmail.com>
2015-08-29 00:03:28 +0300

    issue refs #TP-177 : fixes

--- Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Messaging/ViewModels/MailChimpListViewModel.cs
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Messaging/ViewModels/MailChimpListViewModel.cs
@@ -48,6 +48,40 @@ namespace Teeyoot.Messaging.ViewModels
 
         public virtual string WithdrawTemplate { get; set; }
 
+        public virtual string AllOrderDeliveredTemplate { get; set; }
+
+        public virtual string CampaignIsFinishedTemplate { get; set; }
+
+        public virtual string DefinitelyGoSellerTemplate { get; set; }
+
+        public virtual string DefinitelyGoBuyerTemplate { get; set; }
+
+        public virtual string EditedCampaignTemplate { get; set; }
+
+        public virtual string ExpiredMetMinimumTemplate { get; set; }
+
+        public virtual string ExpiredNotSuccessfullTemplate { get; set; }
+
+        public virtual string ExpiredSuccessfullTemplate { get; set; }
+
+        public virtual string MakeTheOrderTemplate { get; set; }
+
+        public virtual string NewCampaignAdminTemplate { get; set; }
+
+        public virtual string NewOrderBuyerTemplate { get; set; }
+
+        public virtual string NotReachGoalMetMinimumTemplate { get; set; }
+
+        public virtual string RecoverOrdersTemplate { get; set; }
+
+        public virtual string RejectTemplate { get; set; }
+
+        public virtual string Shipped3DayAfterTemplate { get; set; }
+
+        public virtual string TermsConditionsTemplate { get; set; }
+
+        public virtual string WithdrawCompletedTemplate { get; set; }
+
         public virtual string Culture { get; set; }
 
         public virtual IEnumerable<LanguageRecord> AvailableLanguages { get; set; }
