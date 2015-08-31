c0a2f3874b3f2881890611929cfd67306667d73c
Kisslotina <vasiliy.kisslotina@gmail.com>
2015-08-31 18:50:55 +0300

    changes

--- Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Controllers/HomeController.cs
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Controllers/HomeController.cs
@@ -332,7 +332,7 @@ namespace Teeyoot.Module.Controllers
                 order.Country = collection["Country"];
                 order.PhoneNumber = collection["PhoneNumber"];
                 order.Reserved = DateTime.UtcNow;
-                order.TotalPrice = order.TotalPrice + 1.99;
+                order.TotalPrice = order.TotalPrice + _deliverySettingService.GetAllSettings().FirstOrDefault(s => s.State == collection["State"]).DeliveryCost;
                 order.IsActive = true;
                 //order.TranzactionId = result.Target.Id;
 
