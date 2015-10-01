97407bac948341fdb43e5b2beb82de2a13245e29
keinlekan <keinlekan@gmail.com>
2015-10-01 16:38:38 +0300

    issue refs TP-210: add translation

---
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/Controllers/AdminTranslationTextController.cs
@@ -0,0 +1,20 @@
+﻿using Orchard.UI.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Teeyoot.Module.Controllers
+{
+    [Admin]
+    public class AdminTranslationTextController : Controller
+    {
+        // GET: AdminTranslationText
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
