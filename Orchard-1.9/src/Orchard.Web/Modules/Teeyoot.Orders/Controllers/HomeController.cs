5116276d91eba53db64017131c6431d176c91976
Oleg Ivanov <oleg.ivanov.nabusteam@gmail.com>
2015-10-06 14:10:58 +0300

    Filter orders by currency TP-220

--- Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Orders/Controllers/HomeController.cs
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Orders/Controllers/HomeController.cs
@@ -1,20 +1,18 @@
-﻿using Orchard;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Orchard;
 using Orchard.ContentManagement;
+using Orchard.Data;
 using Orchard.DisplayManagement;
 using Orchard.Localization;
 using Orchard.Logging;
-using Orchard.Security;
 using Orchard.Settings;
 using Orchard.UI.Admin;
 using Orchard.UI.Navigation;
 using Orchard.UI.Notify;
 using Orchard.Users.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using Orchard.Data;
 using Teeyoot.Module.Common.Enums;
 using Teeyoot.Module.Models;
 using Teeyoot.Module.Services;
@@ -26,6 +24,7 @@ namespace Teeyoot.Orders.Controllers
     [Admin]
     public class HomeController : Controller
     {
+        private readonly IRepository<CurrencyRecord> _currencyRepository;
         private readonly IRepository<OrderStatusRecord> _orderStatusRepository;
         private readonly IOrderService _orderService;
         private readonly ICampaignService _campaignService;
@@ -41,17 +40,20 @@ namespace Teeyoot.Orders.Controllers
         private dynamic Shape { get; set; }
         // GET: Home
 
-        public HomeController(IRepository<OrderStatusRecord> orderStatusRepository,
-                              IOrderService orderService,
-                              ICampaignService campaignService,
-                              IShapeFactory shapeFactory,
-                              IContentManager contentManager,
-                              ISiteService siteService,
-                              IPayoutService payoutService,
-                              INotifier notifierService,
-                              ITeeyootMessagingService teeyootMessagingService, 
-                              IWorkContextAccessor workContextAccessor)
+        public HomeController(
+            IRepository<CurrencyRecord> currencyRepository,
+            IRepository<OrderStatusRecord> orderStatusRepository,
+            IOrderService orderService,
+            ICampaignService campaignService,
+            IShapeFactory shapeFactory,
+            IContentManager contentManager,
+            ISiteService siteService,
+            IPayoutService payoutService,
+            INotifier notifierService,
+            ITeeyootMessagingService teeyootMessagingService,
+            IWorkContextAccessor workContextAccessor)
         {
+            _currencyRepository = currencyRepository;
             _orderStatusRepository = orderStatusRepository;
             _orderService = orderService;
             _campaignService = campaignService;
@@ -68,19 +70,26 @@ namespace Teeyoot.Orders.Controllers
             cultureUsed = culture == "en-SG" ? "en-SG" : (culture == "id-ID" ? "id-ID" : "en-MY");
         }
 
-
         public Localizer T { get; set; }
-        public ActionResult Index(PagerParameters pagerParameters)
+
+        public ActionResult Index(int? filterCurrencyId, PagerParameters pagerParameters)
         {
-            var orders = _orderService.GetAllOrders().Where(o => o.IsActive && o.CurrencyRecord.CurrencyCulture == cultureUsed).ToList();
+            var orders = _orderService.GetAllOrders()
+                .Where(o => o.IsActive && o.CurrencyRecord.CurrencyCulture == cultureUsed);
+
+            if (filterCurrencyId.HasValue)
+            {
+                var filterCurrency = _currencyRepository.Get(filterCurrencyId.Value);
+                orders = orders.Where(o => o.CurrencyRecord == filterCurrency);
+            }
+
             var orderEntities = new AdminOrderViewModel();
-          
-            
+
             foreach (var item in orders)
             {
                 try
                 {
-                   var it = item.Products.First().CampaignProductRecord.CampaignRecord_Id;
+                    var it = item.Products.First().CampaignProductRecord.CampaignRecord_Id;
                 }
                 catch (Exception)
                 {
@@ -90,17 +99,19 @@ namespace Teeyoot.Orders.Controllers
                 var campaignId = item.Products.First().CampaignProductRecord.CampaignRecord_Id;
                 if (campaignId == null)
                     continue;
-              
+
                 var campaign = _campaignService.GetCampaignById(campaignId);
 
                 try
                 {
 
-                var seller = _contentManager.Query<UserPart, UserPartRecord>().Where(user => user.Id == campaign.TeeyootUserId).List().First();
+                    var seller = _contentManager.Query<UserPart, UserPartRecord>()
+                        .Where(user => user.Id == campaign.TeeyootUserId)
+                        .List()
+                        .First();
 
-                double orderProfit = 0;
+                    double orderProfit = 0;
 
-                
                     foreach (var product in item.Products)
                     {
                         var prof = product.CampaignProductRecord.Price - product.CampaignProductRecord.BaseCost;
@@ -112,38 +123,38 @@ namespace Teeyoot.Orders.Controllers
                         orderProfit = orderProfit + (prof*product.Count);
                         orderProfit = Math.Round(orderProfit, 2);
                     }
-                
 
-                //if (string.IsNullOrWhiteSpace(searchString) || campaign.Title.ToLower().Contains(searchString.ToLower()))
-                //{
-                    orderEntities.Orders.Add(new AdminOrder  {
-                    PublicId = item.OrderPublicId,
-                    Products = item.Products,
-                    Status = item.OrderStatusRecord.Name,
-                    EmailBuyer = item.Email,
-                    CampaignId = campaign.Id,
-                    CampaignName = campaign.Title,
-                    CampaignAlias = campaign.Alias,
-                    Id = item.Id,
-                    Profit = orderProfit,
-                    SellerId = seller != null ? seller.Id : 0,
-                    Payout = item.ProfitPaid,
-                    CreateDate = item.Created,
-                    UserNameSeller = seller != null ? seller.UserName : ""
-                   });
-            //}
-                                }
+
+                    //if (string.IsNullOrWhiteSpace(searchString) || campaign.Title.ToLower().Contains(searchString.ToLower()))
+                    //{
+                    orderEntities.Orders.Add(new AdminOrder
+                    {
+                        PublicId = item.OrderPublicId,
+                        Products = item.Products,
+                        Status = item.OrderStatusRecord.Name,
+                        EmailBuyer = item.Email,
+                        CampaignId = campaign.Id,
+                        CampaignName = campaign.Title,
+                        CampaignAlias = campaign.Alias,
+                        Id = item.Id,
+                        Profit = orderProfit,
+                        SellerId = seller != null ? seller.Id : 0,
+                        Payout = item.ProfitPaid,
+                        CreateDate = item.Created,
+                        UserNameSeller = seller != null ? seller.UserName : "",
+                        Currency = item.CurrencyRecord.ShortName
+                    });
+                    //}
+                }
                 catch (Exception ex)
                 {
 
-                    Logger.Error(ex,campaign.TeeyootUserId + "  ERROOORRRRRRRRRRRR " );
+                    Logger.Error(ex, campaign.TeeyootUserId + "  ERROOORRRRRRRRRRRR ");
                     throw;
                 }
-
-
             }
             //var qwe = new List<SelectListItem>();
-            
+
             //var entriesProjection = orderEntities.Orders.Select(e =>
             //{
             //    return Shape.FaqEntry(
@@ -163,7 +174,7 @@ namespace Teeyoot.Orders.Controllers
             //        );
             //});
             //var pager = new Pager(_siteService.GetSiteSettings(), pagerParameters.Page, pagerParameters.PageSize);
-           
+
             //var pagerShape = Shape.Pager(pager).TotalItemCount(entriesProjection.Count());
 
             var orderStatuses = _orderStatusRepository.Table
@@ -174,10 +185,19 @@ namespace Teeyoot.Orders.Controllers
                 })
                 .ToList();
 
+            var currencies = _currencyRepository.Table
+                .Select(c => new CurrencyItemViewModel
+                {
+                    Id = c.Id,
+                    Name = c.ShortName
+                });
+
             return View("Index", new AdminOrderViewModel
             {
                 DynamicOrders = orderEntities.Orders.ToArray(),
-                OrderStatuses = orderStatuses
+                OrderStatuses = orderStatuses,
+                SelectedCurrencyFilterId = filterCurrencyId,
+                Currencies = currencies
             });
         }
 
@@ -185,20 +205,27 @@ namespace Teeyoot.Orders.Controllers
         {
             var order = _orderService.GetOrderByPublicId(publicId.Trim(' '));
 
-            var products = order.Products.Select(o => new { Name = o.CampaignProductRecord.ProductRecord.Name,
+            var products = order.Products.Select(o => new
+            {
+                Name = o.CampaignProductRecord.ProductRecord.Name,
                 Count = o.Count,
                 Currency = o.CampaignProductRecord.CurrencyRecord.Code,
-                Price = o.CampaignProductRecord.Price + Pricing(o.CampaignProductRecord.ProductRecord.SizesAvailable,o.ProductSizeRecord.Id),
+                Price =
+                    o.CampaignProductRecord.Price +
+                    Pricing(o.CampaignProductRecord.ProductRecord.SizesAvailable, o.ProductSizeRecord.Id),
                 Size = o.ProductSizeRecord.SizeCodeRecord.Name,
-                Color = o.ProductColorRecord == null ? o.CampaignProductRecord.ProductColorRecord.Value : o.ProductColorRecord.Value});
-         
+                Color =
+                    o.ProductColorRecord == null
+                        ? o.CampaignProductRecord.ProductColorRecord.Value
+                        : o.ProductColorRecord.Value
+            });
+
             var totalPrice = order.TotalPriceWithPromo > 0.0 ? order.TotalPriceWithPromo : order.TotalPrice;
             totalPrice += order.Delivery;
 
-            var result = new { products, totalPrice };
+            var result = new {products, totalPrice};
             return Json(result, JsonRequestBehavior.AllowGet);
         }
-
 
         private float Pricing(IList<LinkProductSizeRecord> SizesAvailable, int productSizeRecord)
         {
@@ -209,9 +236,7 @@ namespace Teeyoot.Orders.Controllers
                     sizeC = size.SizeCost;
             }
             return sizeC;
-            
         }
-
 
         public JsonResult GetBuyerInfirmation(int orderId)
         {
@@ -229,16 +254,24 @@ namespace Teeyoot.Orders.Controllers
             }, JsonRequestBehavior.AllowGet);
         }
 
-
         public ActionResult EditStatusPayout(string publicId, double profit, int sellerId)
         {
             var order = _orderService.GetOrderByPublicId(publicId.Trim(' '));
             order.Paid = DateTime.Now.ToUniversalTime();
             var campaignId = order.Products.First().CampaignProductRecord.CampaignRecord_Id;
-            var campaign = _campaignService.GetCampaignById(campaignId) ;
+            var campaign = _campaignService.GetCampaignById(campaignId);
             order.ProfitPaid = true;
             _orderService.UpdateOrder(order);
-            _payoutService.AddPayout(new PayoutRecord { Date = DateTime.Now.ToUniversalTime(), Currency_Id = order.CurrencyRecord.Id, Amount = profit, IsPlus = true, Status = "Completed", UserId = sellerId, Event = publicId.Trim(' ') });
+            _payoutService.AddPayout(new PayoutRecord
+            {
+                Date = DateTime.Now.ToUniversalTime(),
+                Currency_Id = order.CurrencyRecord.Id,
+                Amount = profit,
+                IsPlus = true,
+                Status = "Completed",
+                UserId = sellerId,
+                Event = publicId.Trim(' ')
+            });
             return RedirectToAction("Index");
         }
 
@@ -260,21 +293,31 @@ namespace Teeyoot.Orders.Controllers
             var order = _orderService.GetOrderById(orderId);
             var campId = order.Products.FirstOrDefault().CampaignProductRecord.CampaignRecord_Id;
 
-            if ((order.OrderStatusRecord.Name == OrderStatus.Cancelled.ToString() || order.OrderStatusRecord.Name == OrderStatus.Unapproved.ToString()) && (orderStatus != OrderStatus.Cancelled.ToString() || orderStatus != OrderStatus.Unapproved.ToString()) && order.OrderStatusRecord.Name != orderStatus)
+            if ((order.OrderStatusRecord.Name == OrderStatus.Cancelled.ToString() ||
+                 order.OrderStatusRecord.Name == OrderStatus.Unapproved.ToString()) &&
+                (orderStatus != OrderStatus.Cancelled.ToString() || orderStatus != OrderStatus.Unapproved.ToString()) &&
+                order.OrderStatusRecord.Name != orderStatus)
             {
                 var sum = order.Products.Select(o => o.Count).Sum();
-                var campaign = _campaignService.GetCampaignById(order.Products.FirstOrDefault().CampaignProductRecord.CampaignRecord_Id);
+                var campaign =
+                    _campaignService.GetCampaignById(
+                        order.Products.FirstOrDefault().CampaignProductRecord.CampaignRecord_Id);
                 campaign.ProductCountSold = campaign.ProductCountSold + sum;
             }
 
-            if ((order.OrderStatusRecord.Name != OrderStatus.Cancelled.ToString() || order.OrderStatusRecord.Name != OrderStatus.Unapproved.ToString()) && order.OrderStatusRecord.Name != orderStatus && (orderStatus == OrderStatus.Cancelled.ToString() || orderStatus == OrderStatus.Unapproved.ToString()))
+            if ((order.OrderStatusRecord.Name != OrderStatus.Cancelled.ToString() ||
+                 order.OrderStatusRecord.Name != OrderStatus.Unapproved.ToString()) &&
+                order.OrderStatusRecord.Name != orderStatus &&
+                (orderStatus == OrderStatus.Cancelled.ToString() || orderStatus == OrderStatus.Unapproved.ToString()))
             {
                 var sum = order.Products.Select(o => o.Count).Sum();
-                var campaign = _campaignService.GetCampaignById(order.Products.FirstOrDefault().CampaignProductRecord.CampaignRecord_Id);
+                var campaign =
+                    _campaignService.GetCampaignById(
+                        order.Products.FirstOrDefault().CampaignProductRecord.CampaignRecord_Id);
                 campaign.ProductCountSold = campaign.ProductCountSold - sum;
             }
             int i = 0;
-            OrderStatus newStatus = (OrderStatus)Enum.Parse(typeof(OrderStatus), orderStatus);
+            OrderStatus newStatus = (OrderStatus) Enum.Parse(typeof (OrderStatus), orderStatus);
             if (orderStatus == "Shipped")
             {
                 order.WhenSentOut = DateTime.UtcNow;
@@ -283,7 +326,7 @@ namespace Teeyoot.Orders.Controllers
             var pathToTemplates = Server.MapPath("/Modules/Teeyoot.Module/Content/message-templates/");
             var pathToMedia = Request.Url.Scheme + "://" + Request.Url.Authority + Request.ApplicationPath.TrimEnd('/');
             //ToDo: delete order if new status is "Cancelled"
-            
+
             var ordersForCampaign = _orderService.GetProductsOrderedOfCampaign(campId);
             foreach (var item in ordersForCampaign)
             {
@@ -294,13 +337,12 @@ namespace Teeyoot.Orders.Controllers
             }
             if (i == 0)
             {
-                _teeyootMessagingService.SendAllOrderDeliveredMessageToSeller((order.Products.FirstOrDefault().CampaignProductRecord.CampaignRecord_Id));
+                _teeyootMessagingService.SendAllOrderDeliveredMessageToSeller(
+                    (order.Products.FirstOrDefault().CampaignProductRecord.CampaignRecord_Id));
             }
             _teeyootMessagingService.SendOrderStatusMessage(pathToTemplates, pathToMedia, orderId, orderStatus);
             _notifierService.Information(T("Successfully updated order status "));
             return RedirectToAction("Index");
         }
-
-
     }
 }
