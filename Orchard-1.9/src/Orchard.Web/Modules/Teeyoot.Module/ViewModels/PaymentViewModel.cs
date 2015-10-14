11562e65d028a877d0ea1eccee6fb9b24dd639bd
Oleg Ivanov <oleg.ivanov.nabusteam@gmail.com>
2015-10-14 12:33:23 +0300

    TP-217

--- Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/ViewModels/PaymentViewModel.cs
+++ Orchard-1.9/src/Orchard.Web/Modules/Teeyoot.Module/ViewModels/PaymentViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using Teeyoot.Module.Models;
 
 namespace Teeyoot.Module.ViewModels
@@ -10,23 +6,14 @@ namespace Teeyoot.Module.ViewModels
     public class PaymentViewModel
     {
         public OrderRecord Order { get; set; }
-
         public PromotionRecord Promotion { get; set; }
-
         public string Result { get; set; }
-
         public string ClientToken { get; set; }
-
         public bool CashDeliv { get; set; }
-
         public bool PayPal { get; set; }
-
         public bool Mol { get; set; }
-
         public bool CreditCard { get; set; }
-
         public string CountryName { get; set; }
-
         //
         //
         public string CashOnDeliveryAvailabilityMessage { get; set; }
@@ -42,5 +29,7 @@ namespace Teeyoot.Module.ViewModels
         public string CreditCardNote { get; set; }
         //
         public string CheckoutPageRightSideContent { get; set; }
-    }   
+        public int Country { get; set; }
+        public IEnumerable<CountryItemViewModel> Countries { get; set; }
+    }
 }
